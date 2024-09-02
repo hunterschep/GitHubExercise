86ccb43ddff875bf1f2a7b07ac4b355d7dddfe2e
Hunter S <hunterschep@gmail.com>
2024-09-02 17:25:32 -0400

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Hunter Scheppat on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
